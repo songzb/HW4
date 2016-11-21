b62d312fef11e2b634f11a842b73afced8a8da38
szbhust@gmail.com <szbhust@gmail.com>
2016-11-21 20:36:19 +0000

    fix some bugs

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -7,3 +7,11 @@ Then /^the director of "(.*)" should be "(.*)"$/ do |title_value,director_value|
   movie = Movie.find_by_title(title_value)
   expect(movie.director).to eq director_value
 end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  
+  regexp = /#{e1}.*#{e2}/m  #m make dot match newlines
+  expect(page.body).to match(regexp)
+end
