6a01cb864eff0b8940cc0ecab75ae9154abddb5f
szbhust@gmail.com <szbhust@gmail.com>
2016-11-22 22:37:42 +0000

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rottenpotatoes::Application.routes.draw do
     # root :to => redirect('/movies')
     get 'movies/:id/director' ,to: 'movies#same_director',:as =>  'same_director'
   
+  resources :movies do
+    resources :similar, :controller=> 'movies', :action => 'same_director', :only => [:index]
+  end
   
   #root 'movies#index'
  # resources :movies
    #map '/' to be a redirect to '/movies'
+   get '/' => 'movies#index'
   root :to => redirect('/movies')
+  
 end
